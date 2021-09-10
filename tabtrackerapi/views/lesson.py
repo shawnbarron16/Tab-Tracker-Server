3f83d7ea9ec21f67b7f786f98598636ec7013080
shawnbarron16 <shawnbarron16@gmail.com>
2021-09-10 16:19:34 -0500

    Added create functionality

--- tabtrackerapi/views/lesson.py
+++ tabtrackerapi/views/lesson.py
@@ -1,8 +1,10 @@
 """View module for handling requests for lessons"""
 from django.http import HttpResponseServerError
+from rest_framework.exceptions import ValidationError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
+from rest_framework import status
 from tabtrackerapi.models import Lesson
 
 class LessonView(ViewSet):
@@ -39,6 +41,27 @@ class LessonView(ViewSet):
         
         except Exception as ex:
             return HttpResponseServerError(ex)
+
+    def create(self, request):
+        """Handles POST requests for lesson
+        Returns:
+            Response -- JSON serialized lesson data
+        """
+
+        try:
+            lesson = Lesson.objects.create(
+                user = request.auth.user,
+                lesson_name = request.data['lesson_name'],
+                link = request.data['link'],
+                description = request.data['description'],
+            )
+            serializer = LessonSerializer(
+                lesson, many=False, context={'request': request}
+            )
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
+        except ValidationError as ex:
+            return Response({"reason": ex.message}, status=status.HTTP_400_BAD_REQUEST)
 
 class LessonSerializer(serializers.ModelSerializer):
     """JSON serializer for lessons
